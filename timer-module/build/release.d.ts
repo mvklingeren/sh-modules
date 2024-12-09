658d450d44e79983889fdf066bec6842520b2b1c
Milco van Klingeren <mvklingeren@gmail.com>
2024-12-09 13:37:16 +0100

    -

---
+++ timer-module/build/release.d.ts
@@ -0,0 +1,14 @@
+/** Exported memory */
+export declare const memory: WebAssembly.Memory;
+/**
+ * assembly/index/run
+ */
+export declare function run(): void;
+/**
+ * assembly/index/stop
+ */
+export declare function stop(): void;
+/**
+ * assembly/index/reset
+ */
+export declare function reset(): void;
