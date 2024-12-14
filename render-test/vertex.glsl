bb5a000e57c95001f09534208ebdecbf60e6ad98
Milco van Klingeren <mvklingeren@gmail.com>
2024-12-14 23:04:29 +0100

    -

--- render-test/vertex.glsl
+++ render-test/vertex.glsl
@@ -7,12 +7,14 @@ in vec3 normal;
 uniform mat4 uModelMatrix;
 uniform mat4 uViewMatrix;
 uniform mat4 uProjectionMatrix;
+uniform mat3 uNormalMatrix; // Add this uniform
 
 out vec3 vNormal;
 out vec3 vPosition;
 
 void main() {
-    vNormal = (uModelMatrix * vec4(normal, 0.0)).xyz;
-    vPosition = (uModelMatrix * vec4(position, 1.0)).xyz;
+    // Transform normal using the normal matrix for correct normal orientation
+    vNormal = normalize(uNormalMatrix * normal);
+    vPosition = vec3(uModelMatrix * vec4(position, 1.0));
     gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(position, 1.0);
 }
